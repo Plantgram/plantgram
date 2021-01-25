2ba5d58eda4a5a0b7e83e4fd0bfc7b2ccf166b26
Mark Todd <marktodd.hl@gmail.com>
2021-01-25 20:26:26 +0100

    Example supabase connection

--- src/app/modules/explore/explore.component.ts
+++ src/app/modules/explore/explore.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+import {
+    Component,
+    OnInit,
+} from '@angular/core';
+import { createClient } from '@supabase/supabase-js';
 
 import { new_mock } from '../../../assets/new_mock';
 
@@ -7,6 +13,20 @@ import { new_mock } from '../../../assets/new_mock';
   templateUrl: './explore.component.html',
   styleUrls: ['./explore.component.styl']
 })
-export class ExploreComponent {
+export class ExploreComponent implements OnInit{
   posts = new_mock;
+  supabaseClient: any;
+
+  ngOnInit(){
+    // Create a single supabase client for interacting with your database
+    const {url, key} = environment.supabase;
+    this.supabaseClient = createClient(url, key);
+  }
+
+  async onGetPosts() {
+    const { data, error } = await this.supabaseClient
+      .from('posts')
+      .select(`id, created_at, user_id, title, description`)
+    console.log(data, error,"supabase")
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  supabase: {
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxMTU5Mzk0NiwiZXhwIjoxOTI3MTY5OTQ2fQ.CGoDIFG5K9DV1TnF31i_Ttc-lIHxvvPuJF25joyB3II',
+    url: "https://ptztwgipskartuvfjwna.supabase.co"
+  }
 };
 
 /*
