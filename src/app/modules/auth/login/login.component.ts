76225bb8c7983229ba57f626184ce5748211a2d5
Mark Todd <marktodd.hl@gmail.com>
2021-01-25 22:12:58 +0100

    WIP Testing user signup/login

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,8 +1,11 @@
+import { environment } from 'src/environments/environment';
+
 import { Component } from '@angular/core';
 import {
     FormControl,
     Validators,
 } from '@angular/forms';
+import { createClient } from '@supabase/supabase-js';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +15,7 @@ import {
 export class LoginComponent {
   password = new FormControl('', [Validators.required]);
   email = new FormControl('', [Validators.required, Validators.email]);
-
+  supabaseClient: any;
   isVisible = false;
 
   getErrorMessage() {
@@ -20,5 +23,20 @@ export class LoginComponent {
       return 'You must enter a value';
     }
     return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  ngOnInit(){
+    const {url, key} = environment.supabase;
+    this.supabaseClient = createClient(url, key);
+  }
+
+  async onSignIn(email='anyname@codemonkey.wtf', password='123'){
+    if(email&&password){
+      let { user, error } = await this.supabaseClient.auth.signIn({
+        email,
+        password
+      })
+      console.log(user, error, "signup")
+    }
   }
 }
--- src/app/modules/auth/signup/signup.component.ts
+++ src/app/modules/auth/signup/signup.component.ts
@@ -1,24 +1,44 @@
-import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+import {
+    Component,
+    OnInit,
+} from '@angular/core';
 import {
     FormControl,
     Validators,
 } from '@angular/forms';
+import { createClient } from '@supabase/supabase-js';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.styl']
+  styleUrls: ['./signup.component.styl'],
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
   password = new FormControl('', [Validators.required]);
   email = new FormControl('', [Validators.required, Validators.email]);
 
+  supabaseClient: any;
   isVisible = false;
+
+  ngOnInit() {
+    const { url, key } = environment.supabase;
+    this.supabaseClient = createClient(url, key);
+  }
 
   getErrorMessage() {
     if (this.email.hasError('required')) {
       return 'You must enter a value';
     }
     return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  async onSignUp() {
+    let { user, error } = await this.supabaseClient.auth.signUp({
+      email: 'anyname@codemonkey.wtf',
+      password: '123',
+    });
+    console.log(user, error, 'signup');
   }
 }
