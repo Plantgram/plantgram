dccacfe594a2b7a20fdf767f6a980b495fee4665
Mark Todd <marktodd.hl@gmail.com>
2021-01-28 20:16:15 +0100

    Fix currentUser in Accountservice

--- src/app/core/services/account.service.ts
+++ src/app/core/services/account.service.ts
@@ -1,4 +1,7 @@
-import { BehaviorSubject } from 'rxjs';
+import {
+    BehaviorSubject,
+    Observable,
+} from 'rxjs';
 
 import { Injectable } from '@angular/core';
 import {
@@ -11,15 +14,17 @@ import { environment } from '../../../environments/environment';
 @Injectable({ providedIn: 'root' })
 export class AccountService {
   private supabaseClient;
-  private userSubject = new BehaviorSubject<any>(null);
-  public readonly user$ = this.userSubject.asObservable();
+  private userSubject: BehaviorSubject<User | null>;
+  public readonly currentUser$: Observable<User | null>;
 
   constructor() {
     const { url, key } = environment.supabase;
     this.supabaseClient = createClient(url, key);
+    this.userSubject = new BehaviorSubject(this.supabaseClient.auth.user());
+    this.currentUser$ = this.userSubject.asObservable();
   }
 
-  public get user(): User {
+  public get currentUser(): User | null {
     return this.userSubject.value;
   }
 
@@ -33,10 +38,7 @@ export class AccountService {
     return { user, error };
   }
 
-  async register(credentials: {
-    email: string;
-    password: string;
-  }) {
+  async register(credentials: { email: string; password: string }) {
     const { user, error } = await this.supabaseClient.auth.signUp(credentials);
     this.userSubject.next(user);
     return { user, error };
--- src/app/modules/explore/explore.component.ts
+++ src/app/modules/explore/explore.component.ts
@@ -1,3 +1,4 @@
+import { AccountService } from 'src/app/core/services/account.service';
 import { environment } from 'src/environments/environment';
 
 import {
@@ -11,22 +12,26 @@ import { new_mock } from '../../../assets/new_mock';
 @Component({
   selector: 'app-explore',
   templateUrl: './explore.component.html',
-  styleUrls: ['./explore.component.styl']
+  styleUrls: ['./explore.component.styl'],
 })
-export class ExploreComponent implements OnInit{
+export class ExploreComponent implements OnInit {
   posts = new_mock;
   supabaseClient: any;
 
-  ngOnInit(){
+  constructor(private accountService: AccountService) {}
+
+  ngOnInit() {
     // Create a single supabase client for interacting with your database
-    const {url, key} = environment.supabase;
+    const { url, key } = environment.supabase;
     this.supabaseClient = createClient(url, key);
+
+    console.log(this.accountService.currentUser, 'is logged in');
   }
 
   async onGetPosts() {
     const { data, error } = await this.supabaseClient
       .from('posts')
-      .select(`id, created_at, user_id, title, description`)
-    console.log(data, error,"supabase")
+      .select(`id, created_at, user_id, title, description`);
+    console.log(data, error, 'supabase');
   }
 }
