bd1854cbee6ac6c65fc90ebc1be13a2c94c352ec
Matej Bošnjak <mbosnjak2@outlook.com>
2021-02-03 20:44:29 +0100

    Added follow action to user profile

--- src/app/modules/user/user.component.ts
+++ src/app/modules/user/user.component.ts
@@ -24,23 +24,56 @@ export class UserComponent implements OnInit {
   request: any;
   user: any;
   user_posts: any;
+  isLoggedIn: any;
+  notLoggedInUserProfile: any;
+  isFollowed: any;
+  isNotFollowed: any;
+  sub: any;
 
   constructor(
     private Activatedroute: ActivatedRoute,
     private authService: AuthService,
     private dbService: DatabaseService
-  ) {}
+  ) {
+    this.isLoggedIn = (authService.currentUser !== null) ? true : false;
+  }
 
   ngOnInit(): void {
     this.request = this.Activatedroute.paramMap.subscribe(async (params) => {
       this.id = params.get('id');
+      this.notLoggedInUserProfile = (this.authService.currentUser?.id !== this.id) ? true : false;
       this.user = await this.getUserProfile();
+      this.isFollowed = await this.checkIfSubscriptionExists();
     });
   }
 
   async getUserProfile() {
     const req = await this.dbService.getUserProfile(this.id);
-    console.log(req);
     return req?.body && req.body[0];
+  }
+
+  async subscribe() {
+    try {
+      await this.dbService.subscribe(this.authService.currentUser?.id, this.user.id);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async unSubscribe() {
+    try {
+      await this.dbService.unSubscribe(this.authService.currentUser?.id, this.user.id);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async checkIfSubscriptionExists() {
+    this.sub = await this.dbService.getSubscription(this.authService.currentUser?.id, this.user.id);
+    if (this.sub.body.length > 0) {
+      return true;
+    }
+
+    return false;
   }
 }
