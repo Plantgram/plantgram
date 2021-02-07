96d16d523781bcc5c7de00db39dbff51d47553f6
Matej Bošnjak <mbosnjak2@outlook.com>
2021-02-07 15:13:32 +0100

    Added random images to posts

--- src/app/modules/explore/post/post.component.ts
+++ src/app/modules/explore/post/post.component.ts
@@ -13,14 +13,36 @@ export class PostComponent implements OnInit {
   isLoggedIn: any;
   isAuthor: any;
   isFollowed: any;
+  randomImage: any;
+  imagesArray = [
+    "assets/311458.jpg",
+    "assets/46974.jpg",
+    "assets/114735.jpg",
+    "assets/145685.jpg",
+    "assets/311458.jpg",
+    "assets/796620.jpg",
+    "assets/1011302.jpg",
+    "assets/1122626.jpg",
+    "assets/1169084.jpg",
+    "assets/1233414.jpg",
+    "assets/1408199.jpg",
+    "assets/1578244.jpg",
+    "assets/1599969.jpg",
+    "assets/4273436.jpg",
+    "assets/4621652.jpg",
+    "assets/plant.jpg",
+    "assets/plants-login-1.jpg"
+  ]
+
 
   constructor(private authService: AuthService, private databaseService: DatabaseService) {
-    this.isLoggedIn = authService.currentUser !== null ? true : false;
+    this.isLoggedIn = authService.currentUser !== null ? true : false;    
   }
 
   async ngOnInit(): Promise<void> {
     this.isAuthor = this.isLoggedIn && this.authService.currentUser?.id !== this.post.user_id ? true : false;
     this.isFollowed = this.isLoggedIn ? await this.checkIfSubscriptionExists() : false;
+    this.randomImage = this.imagesArray[Math.floor(Math.random() * this.imagesArray.length)];
   }
 
   async subscribe() {
@@ -50,4 +72,6 @@ export class PostComponent implements OnInit {
   getPostTagList() {
     return this.post.tags && this.post.tags.tagsList ? this.post.tags.tagsList.join(', ') : '';
   }
+
+
 }
